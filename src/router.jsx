a60983997a526c0105894385fb792d018e377fdc
nadiaS11 <nadiasultana.ns11@gmail.com>
2023-10-18 01:48:31 +0600

    first commit

---
+++ src/router.jsx
@@ -0,0 +1,10 @@
+import { createBrowserRouter } from "react-router-dom";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <div className="card-title">Hello world!</div>,
+  },
+]);
+
+export default router;
